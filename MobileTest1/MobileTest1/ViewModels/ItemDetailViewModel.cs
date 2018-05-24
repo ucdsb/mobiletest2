cf6720d68208cf6e53cc8ad35c925c43949aab65
Taggart-Smith, Matthew <matthew.taggart-smith@my.ucdsb.ca>
2018-05-24 17:28:39 +0000

    First build

---
+++ MobileTest1/MobileTest1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileTest1.Models;
+
+namespace MobileTest1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
