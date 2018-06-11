4223964981a7525743043b6094544b4bf0317533
Scott Morrison <scott.morrison@ucdsb.on.ca>
2018-06-11 14:58:50 -0400

    Added App Center SDK guids

--- MobileTest1/MobileTest1/App.xaml.cs
+++ MobileTest1/MobileTest1/App.xaml.cs
@@ -3,6 +3,13 @@ using Xamarin.Forms;
 using MobileTest1.Views;
 using Xamarin.Forms.Xaml;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 [assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace MobileTest1
 {
@@ -13,14 +20,17 @@ namespace MobileTest1
 		{
 			InitializeComponent();
 
-
 			MainPage = new MainPage();
 		}
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts e
-		}
+            // Handle when your app starts e
+            AppCenter.Start("android=b2365228-c7dd-48f4-a8ba-4727e0da1fe1;" +
+                  "ios=b1f9fe31-39d4-4e12-880e-bdd8e707957e;",
+                  typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("android=b2365228-c7dd-48f4-a8ba-4727e0da1fe1;" + "ios=b1f9fe31-39d4-4e12-880e-bdd8e707957e;", typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
